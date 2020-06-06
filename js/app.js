6383f57825b38ada31cb247287bced5f319b1fec
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2020-06-06 16:39:07 +0200

    feat: Linked app.js & build nav list

--- js/app.js
+++ js/app.js
@@ -11,19 +11,23 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
+const sections = document.getElementsByTagName('section');
+const ul = document.getElementById('navbar__list');
 
+console.log(sections);
+console.dir(ul);
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -31,9 +35,21 @@
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
+const buildNav = () => {
+     for (let section of sections) {
+
+        let li = section.attributes['data-nav'].nodeValue;
+        console.log(li);
+
+        ul.innerHTML += `<li><a href="#">${li}</a></li>`;
+
+    }
+}
+
+buildNav();
 
 
 // Add class 'active' to section when near top of viewport
@@ -46,12 +62,10 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
